b4649ff592f29f13467cc1a6564c4af972407c9a
Christian Gilbert <chrisdilbert@gmail.com>
2018-12-19 13:41:33 -0500

    Update test_importer.py

--- exporter/models/test_importer.py
+++ exporter/models/test_importer.py
@@ -19,16 +19,15 @@ class TestExporter(models.Model):
     name = fields.Char()
     root_folder = fields.Char()
     folders_in_root = fields.Text()
-    folder_to_export = fields.Text()
     quantity = fields.Integer()
 
     def zipdir(self,dirPath=None, zipFilePath=None, includeDirInZip=True):
+
         if not os.path.isdir(dirPath):
             raise OSError("dirPath argument must point to a directory. "
                           "'%s' does not." % dirPath)
         parentDir, dirToZip = os.path.split(dirPath)
 
-        # Little nested function to prepare the proper archive path
         def trimPath(path):
             archivePath = path.replace(parentDir, "", 1)
             if parentDir:
@@ -43,37 +42,30 @@ class TestExporter(models.Model):
             for fileName in fileNames:
                 filePath = os.path.join(archiveDirPath, fileName)
                 outFile.write(filePath, trimPath(filePath))
-            # Make sure we get empty directories as well
             if not fileNames and not dirNames:
-                zipInfo = zipfile.ZipInfo(trimPath(archiveDirPath) + "/")
-                # some web sites suggest doing
-                # zipInfo.external_attr = 16
-                # or
-                # zipInfo.external_attr = 48
-                # Here to allow for inserting an empty directory.  Still TBD/TODO.
                 outFile.writestr(zipInfo, "")
         outFile.close()
 
     @api.multi
     @api.model
     def test(self):
-        l = [d for d in os.listdir(self.root_folder) if os.path.isdir(d)]
-        str_dir= ''
-        for d in l:
-            str_dir = str_dir + self.root_folder + '/' +  d + "\n"
-
-        self.write({'folders_in_root': str_dir})
+        try:
+            l = '\n'.join([d for d in os.listdir(self.root_folder) if os.path.isdir(d)])
+            self.write({'folders_in_root': l,'name': 'done'})
+        except Exception as e:
+            raise UserError(e)
 
 
     @api.multi
     @api.model
     def test2(self):
-        
+
         if self.folders_in_root:
             folders= str.splitlines(self.folders_in_root)
             folders_new = str.splitlines(self.folders_in_root)
-            for idx, path in folders:
-                if idx<= self.quantity-1:                    
+            for idx, path in enumerate(folders):
+                if idx<= self.quantity-1:
+
                     try:
                         buff = BytesIO()
                         self.zipdir(dirPath=path, zipFilePath=buff, includeDirInZip=True)
